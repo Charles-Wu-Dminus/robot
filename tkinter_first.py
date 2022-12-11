21843c4362e855cc95024c00525628420ca01f9c
Charles Wu <dminus171013@gmail.com>
2022-12-11 20:44:40 +0800

    windows: Add output display in window feature
    Add output frame and related variables and methods,
    display word segmentation result in output frame.

--- tkinter_first.py
+++ tkinter_first.py
@@ -8,6 +8,13 @@ root = tk.Tk()
 # 設置窗口標題
 root.title("Input Text")
 
+# 新增輸出框架
+output_frame = tk.Frame(root)
+output_frame.pack()
+output_text = tk.StringVar()
+output_label = tk.Label(output_frame, textvariable=output_text)
+output_label.pack()
+
 # 創建一個提交按鈕
 button = tk.Button(root, text="Submit")
 
@@ -25,6 +32,9 @@ def submit(event):
     # 將文本分詞
     words = list(jieba.cut(text))
     print("分詞後的結果為：", words)
+
+     # 將輸出文字顯示在輸出框架中
+    output_text.set("分詞後的結果為：" + " ".join(words))
 
 # 為按鈕綁定事件處理器
 button.bind("<Button-1>", submit)
